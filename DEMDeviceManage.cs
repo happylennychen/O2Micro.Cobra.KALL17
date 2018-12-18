a872537e49d356df27428cffceb58d8312f7e0a3
leon.chen <happylennychen@outlook.com>
2018-12-18 16:37:39 +0800

    1214 release

--- DEMDeviceManage.cs
+++ DEMDeviceManage.cs
@@ -198,7 +198,7 @@ namespace O2Micro.Cobra.KALL17
 
             m_dem_bm.Init(this);
             m_dem_dm.Init(this);
-            LibInfor.AssemblyRegister(Assembly.GetExecutingAssembly(), ASSEMBLY_TYPE.DEM);
+            LibInfor.AssemblyRegister(Assembly.GetExecutingAssembly(), ASSEMBLY_TYPE.OCE);
             LibErrorCode.UpdateDynamicalLibError(ref m_dynamicErrorLib_dic);
         }
 
